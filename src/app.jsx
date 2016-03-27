fef1f41c9982c7592ffa495332a8044535ef0ac2
TMiguelT <ttmigueltt@gmail.com>
2016-03-28 03:37:35 +1100

    Fixed spinner position

--- src/app.jsx
+++ src/app.jsx
@@ -23,18 +23,38 @@ new Vue({
     ready(){
 
         //Show the spinner
-        new Spinner({color:'#fff', lines: 12}).spin(this.$els.spin);
+        const spinner = new Spinner({
+            lines: 13 // The number of lines to draw
+            , length: 28 // The length of each line
+            , width: 14 // The line thickness
+            , radius: 42 // The radius of the inner circle
+            , scale: 0.5 // Scales overall size of the spinner
+            , corners: 1 // Corner roundness (0..1)
+            , color: '#000' // #rgb or #rrggbb or array of colors
+            , opacity: 0.25 // Opacity of the lines
+            , rotate: 0 // The rotation offset
+            , direction: 1 // 1: clockwise, -1: counterclockwise
+            , speed: 1 // Rounds per second
+            , trail: 60 // Afterglow percentage
+            , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+            , zIndex: 2e9 // The z-index (defaults to 2000000000)
+            , className: 'spinner' // The CSS class to assign to the spinner
+            , top: '50%' // Top position relative to parent
+            , left: '50%' // Left position relative to parent
+            , shadow: false // Whether to render a shadow
+            , hwaccel: false // Whether to use hardware acceleration
+            , position: 'absolute' // Element positioning
+        }).spin();
+        this.$els.spin.appendChild(spinner.el);
 
         //Load the cards
-        require.ensure(['./AllCards.json'], require => {
-            const MtgJson = require('./AllCards.json');
-
+        require(["./AllCards.json"], (MtgJson) => {
             this.loading = false;
             this.groupedCards = _.chain(MtgJson)
                 .filter(card => 'types' in card && card.types.indexOf('Creature') != -1)
                 .groupBy('cmc')
                 .value();
-        })
+        });
     },
     methods: {
         generate(){
@@ -56,7 +76,7 @@ new Vue({
                 var successful = document.execCommand('cut');
                 var msg = successful ? 'successful' : 'unsuccessful';
                 console.log('Cutting text command was ' + msg);
-            } catch(err) {
+            } catch (err) {
                 console.log('Oops, unable to cut');
             }
 
